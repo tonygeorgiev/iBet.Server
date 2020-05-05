6affd4b0a478cce08c3a95c8c25236563bbcedb6
Tony Georgiev <tonygeorgiew@gmail.com>
2020-05-05 12:49:53 +0300

    solution added

---
+++ iBet.Server/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿namespace iBet.Server.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+    }
+}
---
+++ iBet.Server/Startup.cs
@@ -0,0 +1,41 @@
+namespace iBet.Server
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    using iBet.Server.Data;
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) => this.Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddControllers();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDatabaseErrorPage();
+            }
+
+            app.UseRouting(); 
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
